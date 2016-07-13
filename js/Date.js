92016dff48a409bdb7c2691e1fe001139cf9e428
lytwaytLaz <laszlo.hago@gmail.com>
2016-07-13 15:53:50 +0200

    HTML assignments

---
+++ js/Date.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Elev1 on 2016-07-13.
+ */
+
+var d = new Date();
+var d = new Date(milliseconds);
+var d = new Date(dateString);
+var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+
+
+alert ('Hello world');
+
+
+function count_rabbits() {
+    for(var i=1; i<=3; i++) {
+        // operator + concatenates strings
+        alert("Rabbit "+i+" out of the hat!")
+    }
+}
